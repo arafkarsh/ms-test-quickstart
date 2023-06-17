f0b2ee431fea233b9665d5c911c1c66c9745f477
Araf Karsh Hamid <araf.karsh@metamagic.in>
2023-06-17 12:11:54 +0530

    Added Spock Examples

---
+++ src/test/java/test/fusion/water/order/spock/spock/tests/OrderExpectWhereSpec.groovy
@@ -0,0 +1,173 @@
+/**
+ * (C) Copyright 2023 Araf Karsh Hamid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test.fusion.water.order.spock.spock.tests
+
+import spock.lang.Specification
+import spock.lang.Stepwise
+import java.time.Month
+
+import io.fusion.water.order.domainLayer.models.OrderEntity
+import io.fusion.water.order.domainLayer.models.Customer
+import io.fusion.water.order.utils.Utils;
+
+/**
+ * Spock - Order Parametrized Test Suite
+ *
+ * Using EXPECT / WHERE Clauses
+ *
+ * @author arafkarsh
+ *
+ */
+@Stepwise
+class OrderExpectWhereSpec extends Specification {
+
+    def order
+    def counter = 1
+
+    def setupSpec() {
+        println("== Order Parametrized Suite Execution Started...")
+    }
+
+    def setup() {
+        println("${counter}. Create an Empty Order..")
+        order = new OrderEntity()
+    }
+
+    /**
+     * In Example 1.1 the Spock testing framework is treating each phone number as a separate iteration of the test
+     * due to the where: block. This results in the test being run once for each phone number. In your test report,
+     * you will see the test being reported as executed three times, once for each phone number. This can be beneficial
+     * for isolating failures to a specific iteration of the test.
+     */
+    def "1.1 Value Source : String Array - 1"() {
+        expect:
+            def order = new OrderEntity.Builder()
+                    .addCustomer(new Customer("UUID", "John", "Doe", phoneNumber))
+                    .build()
+            order.isCustomerAvailable() == true
+            order.getCustomer().getPhoneList().size() == 1
+        where:
+            phoneNumber << ["0123456777", "0123456888", "0123456999"]
+    }
+
+    /**
+     * In Example 1.2, you are using Groovy's each loop to iterate over the phone numbers within a single test.
+     * This means that the test is executed only once, but it performs the check for each phone number within
+     * that single execution. If a failure occurs, it will be harder to identify which iteration (i.e., which
+     * phone number) caused the failure, since they are all part of the same test execution.
+     *
+     * NOT RECOMMENDED
+     */
+    def "1.2. Value Source - String Array - 2"() {
+        expect:
+        ["0123456777", "0123456888", "0123456999"]
+                .each { phoneNumber ->
+                    order = new OrderEntity.Builder()
+                            .addCustomer(
+                                    new Customer("UUID", "John", "Doe", phoneNumber))
+                            .build()
+                    order.isCustomerAvailable() == true
+                    order.getCustomer().getPhoneList().size() == 1
+                }
+    }
+
+    def "1.3 Value Source - Number Array"() {
+        expect:
+            Utils.Numbers.isOdd(number)
+        where:
+            number << [1, 3, 5, -3, 15, Integer.MAX_VALUE]
+    }
+
+    def "1.4 Value Source - Null Source"() {
+        expect:
+            Utils.Strings.isBlank(input)
+        where:
+            input << [null]
+    }
+
+    def "1.5 Value Source - Empty Source"() {
+        expect:
+            Utils.Strings.isBlank(input)
+        where:
+            input << [""]
+    }
+
+    def "1.6 Value Source - Null & Empty Source"() {
+        expect:
+            Utils.Strings.isBlank(input)
+        where:
+            input << [null, ""]
+    }
+
+    def "1.7 Value Source - Null & Empty Source & White spaces"() {
+        expect:
+            Utils.Strings.isBlank(input)
+        where:
+            input << [null, "", "  ", "\t", "\n"]
+    }
+
+    def "2.1 Enum Source - 12 Months"() {
+        expect:
+            monthNumber > 0 && monthNumber < 13
+        where:
+            month << Month.values()
+            monthNumber = month.getValue()
+    }
+
+    def "2.2 Enum Source - 4 Months"() {
+        expect:
+            month.length(isALeapYear) == 30
+        where:
+            month << [Month.APRIL, Month.JUNE, Month.SEPTEMBER, Month.NOVEMBER]
+            isALeapYear = false
+    }
+
+    def "2.3 Method Source - Phone No. should match the Format"() {
+        expect:
+            def order = new OrderEntity.Builder()
+                    .addCustomer(new Customer("UUID", "John", "Doe", phoneNumber))
+                    .build()
+            order.isCustomerAvailable() == true
+            order.getCustomer().getPhoneList().size() == 1
+        where:
+            phoneNumber << phoneNumberList()
+    }
+
+    def "2.4. Should Test Order Creation Repeatedly - 3 Times"() {
+        expect:
+        3.times {
+            order = new OrderEntity.Builder()
+                    .addCustomer(
+                            new Customer("UUID", "John", "Doe", "0123456789"))
+                    .build()
+            order.isCustomerAvailable() == true
+        }
+    }
+
+    private static List<String> phoneNumberList() {
+        ["0123456789", "0123456798", "0123456897"]
+    }
+
+    def cleanup() {
+        println("${counter}. Should Execute After Each Test")
+        counter++
+    }
+
+    def cleanupSpec() {
+        println("== Order Parametrized Suite Execution Completed...")
+    }
+
+}
---
+++ src/test/java/test/fusion/water/order/spock/spock/tests/OrderGivenWhenThenSpec.groovy
@@ -0,0 +1,142 @@
+/**
+ * (C) Copyright 2023 Araf Karsh Hamid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test.fusion.water.order.spock.spock.tests
+
+import spock.lang.Specification
+
+import io.fusion.water.order.domainLayer.models.OrderEntity
+import io.fusion.water.order.domainLayer.models.Customer
+import spock.lang.Stepwise
+
+/**
+ * Spock - Order Specs Suite
+ *
+ * USING GIVEN / WHEN / THEN
+ *
+ * or Setup / Action / Result
+ *
+ * @author arafkarsh
+ *
+ */
+@Stepwise
+class OrderGivenWhenThenSpec extends Specification {
+
+    OrderEntity order
+
+    def setupSpec() {
+        println "== Order tests Suite Execution Started..."
+    }
+
+    def setup() {
+        println "Create an Empty Order.."
+        order = new OrderEntity()
+    }
+
+    def "1. Should Create Customer if Customer is Available"() {
+        given: "A New Customer"
+            Customer c = new Customer("UUID", "John", "Doe", "0123456789")
+
+        when: "an order is created with the customer"
+            order = new OrderEntity.Builder().addCustomer(c).build()
+
+        then: "the order has a customer"
+            order.isCustomerAvailable() == true
+    }
+
+    def "2. Should Throw RuntimeException When FirstName Is Null"() {
+        given: "A Customer with Null First Name"
+            Customer c = new Customer("UUID", null, "Doe", "0123456789")
+
+        when: "Adding a Customer with Null First Name"
+            order = new OrderEntity.Builder()
+                    .addCustomer(c)
+                    .build()
+        then: "An Exception is thrown"
+            thrown RuntimeException
+    }
+
+    def "3. Should Throw RuntimeException When LastName Is Null"() {
+        given: "A Customer with Null Last Name"
+            Customer c = new Customer("UUID", "John", null, "0123456789")
+
+        when: "Adding a Customer with Null Last Name"
+            order = new OrderEntity.Builder().addCustomer(c).build()
+
+        then: "An Exception is thrown"
+            thrown RuntimeException
+    }
+
+    def "4. Should Throw RuntimeException When PhoneNumber Is Null"() {
+        given: "A Customer with Null Phone Number"
+            Customer c = new Customer("UUID", "John", "Doe", null)
+
+        when: "Adding a Customer with Null Phone Number"
+            order = new OrderEntity.Builder().addCustomer(c).build()
+
+        then: "An Exception is thrown"
+            thrown RuntimeException
+    }
+
+    /**
+     * Data Driven Test
+     * @param a
+     * @param b
+     * @param max
+     * @return
+     */
+    def "5. Maximum of two numbers"(int a, int b, int max) {
+        given: "two numbers"
+        // defined by the where block
+
+        when: "Math.max is called"
+            def result = Math.max(a, b)
+
+        then: "the result is the maximum of the two numbers"
+            result == max
+
+        where:
+        a | b || max
+        1 | 3 || 3
+        7 | 4 || 7
+        0 | 0 || 0
+    }
+
+    /**
+     * Conditional test execution based on the iteration
+     * Spock allows you to control the execution of your test depending on the iteration of data-driven testing
+     * @return
+     */
+    def "6. only runs on certain iterations <=8"() {
+        given: "A Set of Numbers"
+        // defined by the where block
+        when:
+            println x
+
+        then:
+            1 == 1
+
+        where: "only run when x <= 3"
+        x << [1, 3, 4, 7, 9, 10, 12, 13, 15, 19].findAll { it <= 8 }
+    }
+
+    def cleanup() {
+        println "Should Execute After Each Test"
+    }
+
+    def cleanupSpec() {
+        println "== Order tests Suite Execution Completed..."
+    }
+}
