55fe3774a239ecad4051365adbb4826ca534f6e6
Araf Karsh Hamid <araf.karsh@metamagic.in>
2024-12-11 16:57:06 +0530

    Added Diffblue Cover AI Test Generation...

--- code-delta-lc.sh
+++ code-delta-lc.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# -------------------------------------------------------------------------------------------
+# @author: Araf Karsh Hamid
+# -------------------------------------------------------------------------------------------
 
 echo "Delta Line Count"
 git log  --pretty=tformat: --numstat | grep src | perl -ane'$i += $F[0]; $d += $F[1]; $c = $i-$d; END{ print "Added lines: $i Removed lines: $d Current lines: $c\n"}'
